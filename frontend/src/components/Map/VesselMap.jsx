d5011c5cc7f7f4ea1a2faadbfbc803cc09008ba3
ayush-singh-0601 <singhayush062006@gmail.com>
2026-08-30 18:11:43 +0530

    fix(map): align MapContainer and TileLayer maxZoom to 19 to prevent zoom control/tile mismatch

--- frontend/src/components/Map/VesselMap.jsx
+++ frontend/src/components/Map/VesselMap.jsx
@@ -126,7 +126,7 @@ export default function VesselMap({ vessels = [], selectedVessel, onVesselClick 
       center={[20, 0]}
       zoom={3}
       minZoom={2}
-      maxZoom={18}
+      maxZoom={19}
       zoomControl={true}
       className="vessel-map"
       worldCopyJump={true}
@@ -136,7 +136,7 @@ export default function VesselMap({ vessels = [], selectedVessel, onVesselClick 
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
         subdomains="abcd"
-        maxZoom={20}
+        maxZoom={19}
       />
 
       <MapViewportTracker onViewportChange={setViewport} />
